362ef645aefcaef2b55d7b4cd56810c5a07c4e1a
SINHEESEOP <shfurwnd97@gmail.com>
2025-04-07 13:37:17 +0900

    project init

---
+++ src/main/java/com/samsungnomads/wheretogo/WhereToGoApplication.java
@@ -0,0 +1,14 @@
+package com.samsungnomads.wheretogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+    public class WhereToGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereToGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samsungnomads/wheretogo/WhereToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsungnomads.wheretogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereToGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

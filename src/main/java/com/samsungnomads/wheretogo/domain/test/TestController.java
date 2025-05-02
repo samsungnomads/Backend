e6b094a0aa6e4ba12af93ac03d31e189fe008467
Kim NamHoon <southernlight3816@gmail.com>
2025-05-02 13:09:56 +0900

    feat : TestController 에 /auth 추가로 @AuthenticationPrincipal 이 잘 동작하는지 확인

--- src/main/java/com/samsungnomads/wheretogo/domain/test/TestController.java
+++ src/main/java/com/samsungnomads/wheretogo/domain/test/TestController.java
@@ -2,11 +2,15 @@ package com.samsungnomads.wheretogo.domain.test;
 
 import com.samsungnomads.wheretogo.global.response.SuccessCode;
 import com.samsungnomads.wheretogo.global.response.SuccessResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("api/test")
 public class TestController implements TestControllerDocs {
@@ -20,6 +24,12 @@ public class TestController implements TestControllerDocs {
     public ResponseEntity<SuccessResponse<TestResponseDto>> successWithData() {
         TestResponseDto testResponseDto = new TestResponseDto("테스트 메시지 입니다", 123);
         return SuccessResponse.of(SuccessCode.OK, testResponseDto);
+    }
+
+    @GetMapping("/auth")
+    public ResponseEntity<SuccessResponse> auth(@AuthenticationPrincipal UserDetails userDetails) {
+        log.info(userDetails.getUsername());
+        return SuccessResponse.of(SuccessCode.OK);
     }
 
     @GetMapping("123")

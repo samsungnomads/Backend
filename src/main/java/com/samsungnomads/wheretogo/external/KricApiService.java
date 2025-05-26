5139c15a81476337050e83cd1976955b4b01ca53
Kim NamHoon <southernlight3816@gmail.com>
2025-05-26 18:51:13 +0900

    chore : KricApiService 에 url key 값 확인용 로그 추가

--- src/main/java/com/samsungnomads/wheretogo/external/KricApiService.java
+++ src/main/java/com/samsungnomads/wheretogo/external/KricApiService.java
@@ -58,6 +58,9 @@ public class KricApiService {
 
             if (responseDto.getBody() == null) {
                 log.warn("[KRIC] 응답 body, List<StationInfo>가 null 입니다. lineCode: {}", line.getCode());
+                log.warn("[KRIC] kricApiUrl {}", kricApiUrl);
+                log.warn("[KRIC] kricApiKey {}", kricApiKey);
+
                 return List.of();
             }
 
